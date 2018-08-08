ddd53c7f56e5c5b015b1847097373c064a979190
Ecksy <bailesbraden@hotmail.com>
2018-08-07 23:03:58 -0500

    Practice files

---
+++ first_Practice.py
@@ -0,0 +1,7 @@
+person_age = "34"
+person_age2 = "25"
+person_name = "Tom"
+
+print (person_name + " is " + person_age + " years old")
+print ("But " + person_name + " wants to be " + person_age2)
+print (person_name + " is " + person_age)
